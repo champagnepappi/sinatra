5f4b9e9287c5f63e0e816a77b9b9ba1cc15ef74d
kevin <kevinochieng548@gmail.com>
2016-10-06 12:47:58 +0300

    specify things to happen prior to handling the request

--- game.rb
+++ game.rb
@@ -1,5 +1,15 @@
 require 'sinatra'
 
+#before we process a route, we'll set the response as
+#plain text and setup an array of viable moves that
+#a player (and computer) can perform
+
+before do
+  content_type :txt
+  @defeat = {rock: :scissors, paper: :rock, scissors: :paper}
+  @throws = @defeat.keys
+end
+
 get '/throw/:type' do
   #play here
 end
