fff0525dba8f98519f076846d5a30ea26957b6f6
kevin <kevinochieng548@gmail.com>
2016-10-16 12:31:01 +0300

    write simplified equivalent of Sinatra application using Rack

---
+++ rack.rb
@@ -0,0 +1,18 @@
+module MySinatra
+  class Application
+    def self.call(env)
+      new.call(env)
+    end
+
+    def call(env)
+      headers = {'Content-Type' => 'text/html'}
+      if env['PATH_INFO'] == '/'
+        status, body =200, 'hi'
+      else
+        status, body =404, "SINATRA doesn't know this ditty"
+      end
+      headers['Content-Length'] = body.length.to_s
+      [status, headers, [body]]
+    end
+  end
+end
